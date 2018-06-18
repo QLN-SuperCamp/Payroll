4659503537971256296ad5876469143af808c94a
Alex <alexanderlee58col@gmail.com>
2018-06-18 09:55:03 -0700

    Added all other staff members
    hourly only

--- assets/js/staffmembers.js
+++ assets/js/staffmembers.js
@@ -1,5 +1,4 @@
-var staffMembersAll = [
-    {
+var staffMembersAll = [{
         firstName: "Jules",
         lastName: "Damey",
         campsWorking: ["sw-camp1", "sw-camp2"],
@@ -23,7 +22,7 @@ var staffMembersAll = [
     {
         firstName: "Yolanda",
         lastName: "Drew",
-        campsWorking: ["sw-camp1", "sw-camp2"],
+        campsWorking: ["sw-camp1", "sw-camp2", "stan-camp5", "stan-camp6"],
         position1: "LC",
         position2: ""
     },
@@ -35,10 +34,346 @@ var staffMembersAll = [
         position2: ""
     },
     {
-        firstName: "Alex (OC Test)",
-        lastName: "Lee (OC Test)",
-        campsWorking: ["sw-camp1", "sw-camp2"],
-        position1: "OC",
+        firstName: "Jamila",
+        lastName: "Ford",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3", "stan-camp4", "stan-camp5"],
+        position1: "STL",
         position2: ""
+    },
+    {
+        firstName: "Divine",
+        lastName: "Rolle",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3", "stan-camp4", "stan-camp5", "stan-camp6"],
+        position1: "TL",
+        position2: ""
+    },
+    {
+        firstName: "Noah",
+        lastName: "Fedler",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3", "stan-camp4"],
+        position1: "TL",
+        position2: ""
+    },
+    {
+        firstName: "Trenton",
+        lastName: "Barnett",
+        campsWorking: ["stan-camp1", "stan-camp3", "stan-camp5", "stan-camp6"],
+        position1: "TL",
+        position2: ""
+    },
+    {
+        firstName: "Desmond",
+        lastName: "Yalom",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3", "stan-camp4", "stan-camp5"],
+        position1: "TL",
+        position2: ""
+    },
+    {
+        firstName: "Douglas",
+        lastName: "Hearns",
+        campsWorking: ["stan-camp1"],
+        position1: "TL",
+        position2: ""
+    },
+    {
+        firstName: "Derrick",
+        lastName: "Celestine",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3", "stan-camp4"],
+        position1: "TL",
+        position2: ""
+    },
+    {
+        firstName: "Jimmy",
+        lastName: "McDonald",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3"],
+        position1: "TL",
+        position2: ""
+    },
+    {
+        firstName: "Claire",
+        lastName: "Levenson",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3", "stan-camp4", "stan-camp5", "stan-camp6"],
+        position1: "TL",
+        position2: ""
+    },
+    {
+        firstName: "Elise",
+        lastName: "Simmons",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3", "stan-camp4", "stan-camp5", "stan-camp6"],
+        position1: "TL",
+        position2: ""
+    },
+    {
+        firstName: "Nina",
+        lastName: "Fleck",
+        campsWorking: ['stan-camp1', 'stan-camp2', 'stan-camp3', 'stan-camp4', 'stan-camp5', 'stan-camp6'],
+        position1: "TL",
+        position2: ""
+    },
+    {
+        firstName: "Alina (Tete)",
+        lastName: "Briley",
+        campsWorking: ["stan-camp1", "stan-camp2"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Kate",
+        lastName: "Ward",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3", "stan-camp4", "stan-camp5"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Erin",
+        lastName: "Janov",
+        campsWorking: ["stan-camp1", "stan-camp3", "stan-camp4", "stan-camp5"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Frederique",
+        lastName: "Desrosiers",
+        campsWorking: ["stan-camp1"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Alexandra",
+        lastName: "Desrosiers",
+        campsWorking: ["stan-camp1"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Nikta",
+        lastName: "Shahbaz",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3", "stan-camp5", "stan-camp6"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Raqqayah",
+        lastName: "Simmons",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3", "stan-camp4"],
+        position1: "LC",
+        position2: ''
+    },
+    {
+        firstName: "Flora",
+        lastName: "Moore",
+        campsWorking: ["stan-camp1", "stan-camp2", "stan-camp3"],
+        position1: "OC",
+        position2: ''
+    },
+    {
+        firstName: "Elizabeth",
+        lastName: "Fung",
+        campsWorking: ["stan-camp4", "stan-camp5", "stan-camp6"],
+        position1: "OC",
+        position2: ''
+    },
+    {
+        firstName: "Shayla",
+        lastName: "Nary",
+        campsWorking: ['stan-camp1', 'stan-camp2', 'stan-camp3', 'stan-camp4', 'stan-camp5'],
+        position1: "WP",
+        position2: ''
+    },
+    {
+        firstName: "Luis",
+        lastName: "Luevanos",
+        campsWorking: ["stan-camp2", "stan-camp3", "stan-camp4", "stan-camp5"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Michael",
+        lastName: "Andrade",
+        campsWorking: ['stan-camp2', 'stan-camp3', 'stan-camp4', 'stan-camp5', 'stan-camp6'],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Molly",
+        lastName: "Brantingham",
+        campsWorking: ["stan-camp2", "stan-camp3", "stan-camp4"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Isabella",
+        lastName: "Borbolla",
+        campsWorking: ["stan-camp2", "stan-camp3"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Lauren",
+        lastName: "Ihle",
+        campsWorking: ["stan-camp2", "stan-camp3", "stan-camp4"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Laci",
+        lastName: "Jackson",
+        campsWorking: ["stan-camp4", "stan-camp5", "stan-camp6"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Nathan",
+        lastName: "Tung",
+        campsWorking: ["stan-camp4", "stan-camp5"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Nathan",
+        lastName: "Tung",
+        campsWorking: ["stan-camp6"],
+        position1: "STL",
+        position2: ''
+    },
+    {
+        firstName: "Jennifer",
+        lastName: "Gerardi",
+        campsWorking: ["stan-camp3", "stan-camp4", "stan-camp5", "stan-camp6"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Alexis",
+        lastName: "Hagan",
+        campsWorking: ["stan-camp5"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+        firstName: "Isabella",
+        lastName: "Stenz",
+        campsWorking: ["stan-camp5", "stan-camp6"],
+        position1: "TL",
+        position2: ''
+    },
+    {
+    firstName: "Alexis",
+    lastName: "Hagan",
+    campsWorking: ["ucf-camp1", "ucf-camp2"],
+    position1: "STL",
+    position2: ''
+    },
+    {
+    firstName: "Alex",
+    lastName: "Abbott",
+    campsWorking: ["ucf-camp1", "ucf-camp2"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Charles",
+    lastName: "Garza",
+    campsWorking: ["ucf-camp1", "ucf-camp2"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Laci",
+    lastName: "Jackson",
+    campsWorking: ["ucf-camp1", "ucf-camp2"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Kayla",
+    lastName: "Jackson",
+    campsWorking: ["ucf-camp1", "ucf-camp2"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Alicyn",
+    lastName: "Kitamura",
+    campsWorking: ["ucf-camp1", "ucf-camp2"],
+    position1: "LC",
+    position2: ''
+    },
+    {
+    firstName: "Luzmaria",
+    lastName: "Estalla",
+    campsWorking: ["ucf-camp1", "ucf-camp2"],
+    position1: "WP",
+    position2: ''
+    },
+    {
+    firstName: "Isabella",
+    lastName: "Stenz",
+    campsWorking: ["villa-camp1", "villa-camp2"],
+    position1: "STL",
+    position2: ''
+    },
+    {
+    firstName: "Trace",
+    lastName: "Craver",
+    campsWorking: ["villa-camp1", "villa-camp2"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Adam",
+    lastName: "Chan",
+    campsWorking: ["villa-camp1", "villa-camp2"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Maxwell",
+    lastName: "Little",
+    campsWorking: ["villa-camp1"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Katrina",
+    lastName: "Hoefflinger",
+    campsWorking: ["villa-camp1", "villa-camp2"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Deja",
+    lastName: "Washington",
+    campsWorking: ["villa-camp1", "villa-camp2"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Rachel",
+    lastName: "Dillman",
+    campsWorking: ["villa-camp1"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Kelly",
+    lastName: "Castleberry",
+    campsWorking: ["villa-camp1"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Viva",
+    lastName: "Sandoval",
+    campsWorking: ["villa-camp1"],
+    position1: "TL",
+    position2: ''
+    },
+    {
+    firstName: "Jordan",
+    lastName: "Wesson",
+    campsWorking: ["villa-camp1", "villa-camp2"],
+    position1: "WP",
+    position2: ''
     }
 ];
