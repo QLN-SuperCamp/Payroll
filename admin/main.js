4156d8b735d83a5041bbab9fa29e9c8b3f5131b6
Alex <alexanderlee58col@gmail.com>
2018-07-16 15:45:07 -0700

    Attempting to change paths to be able to access on web not local computer

--- admin/main.js
+++ admin/main.js
@@ -64,7 +64,7 @@ var body2TXSalary = document.getElementById('body-payPeriod2-TX-salary');
 window.addEventListener('DOMContentLoaded', init);
 
 // Write the time of the CampMinder report
-var file = "reportInfo.JSON";
+var file = "admin/reportInfo.JSON";
 var date = "";
 
 
@@ -75,7 +75,7 @@ $.get(file, function (info) {
 
 
 // CampMinder Report - All 2018 Staff Members
-var report = "2018staff.csv";
+var report = "admin/2018staff.csv";
 var reportArray = [];
 $.get(report, function (data) {
     var csvdata = Papa.parse(data, {
