9468c8406a18c430e8230eb837689ff03a7bb434
Alex <alexanderlee58col@gmail.com>
2018-08-01 06:35:05 -0700

    Added span for Yolanda Drew Special Camp

--- assets/js/main.js
+++ assets/js/main.js
@@ -613,6 +613,8 @@ function addStaffMembers() {
                             return "<em>(" + person.specialStaffMember[0].camps["villa-camp1"] + ")</em> " + person.firstName + " " + person.lastName;
                         } else if (currentCamp === "sw-camp2") {
                             return "<em>(" + person.specialStaffMember[0].camps["sw-camp2"] + ")</em> " + person.firstName + " " + person.lastName;
+                        } else if (currentCamp === "ucf-camp2") {
+                            return "<em>(" + person.specialStaffMember[0].camps["ucf-camp2"] + ")</em> " + person.firstName + " " + person.lastName;
                         }
                     } else if (specificSpecialPerson[0].id === "Nathan Tung") {
                         if (currentCamp === "stan-camp3") {
