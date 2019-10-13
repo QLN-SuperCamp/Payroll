078c0008053e4c902b7c010616221b12ffc6f1ae
alexlee-dev <alex@alexlee.dev>
2019-10-13 14:06:42 -0700

    Remove connect at SiteInformtaion

--- src/components/siteInformation.js
+++ src/components/siteInformation.js
@@ -5,9 +5,8 @@ import NameField from "./nameField";
 import SiteField from "./siteField";
 import CampField from "./campField";
 import DateField from "./dateField";
-import { connect } from "react-redux";
 
-const SiteInformation = ({ firstName, lastName, site, camp, date }) => {
+const SiteInformation = () => {
   return (
     <Paper className={siteInformationStyles.container}>
       <Box className={siteInformationStyles.topContainer}>
@@ -25,12 +24,4 @@ const SiteInformation = ({ firstName, lastName, site, camp, date }) => {
   );
 };
 
-const mapStateToProps = ({ data }) => ({
-  firstName: data.firstName,
-  lastName: data.lastName,
-  site: data.site,
-  camp: data.camp,
-  date: data.date
-});
-
-export default connect(mapStateToProps)(SiteInformation);
+export default SiteInformation;
